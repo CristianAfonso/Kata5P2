d0f35187097aaad37930b9f39cc4dd35625f2a71
can97 <can97@DESKTOP-ETFFE43>
2020-12-02 18:33:18 +0000

    Kata4 v2 con CIPO

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -10,16 +10,31 @@ import java.util.List;
 
 public class Kata4 {
 
-   
     public static void main(String[] args) throws IOException {
+        Kata4 kata4 = new Kata4();
+        kata4.control();
+    }
+    
+    private List<Mail> mailList;
+    private Histogram<String> histogram;
+    
+    private void control() throws IOException {
+        input();
+        process();
+        output();
+    }
+    
+    private void input() throws IOException{
         String fileName = new String("email.txt");
-        
-        List<Mail> mailList = MailListReader.read(fileName);
-        
-        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
-       
+        mailList = MailListReader.read(fileName);
+    }
+    
+    private void process(){
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+    
+    private void output(){
         HistogramDisplay histogramDisplay = new HistogramDisplay("HISTOGRAM",histogram);
         histogramDisplay.execute();
     }
-    
 }
